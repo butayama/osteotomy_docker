149b09f9f8a6d7025565f1c33d5189c645ad49ea
butayama <butayama@users.noreply.github.com>
2021-09-17 20:44:34 +0200

    adding SSL support https still not working

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -152,7 +152,7 @@ accesslog = '-'
 access_log_format = '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"'
 
 # adding SSL support
-certfile = '/ssl/certs/chain_123.crt'
+certfile = '/ssl/certs/chain_23.crt'
 keyfile = '/ssl/certs/osteotomy.eu.key' #problematisch, da key-file nicht in einem versteckten Verzeichnis
 
 # for using letsencrypt
